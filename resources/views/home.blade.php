d265887f26c0ae24f5c6dd966a4838f71b7b9f23
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-23 15:54:02 +0200

    test for images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
                 <div class="comics">
                     @foreach ($comics as $comic)
                     <div class="card">
-                        {{-- <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="Dc Logo"/> --}}
+                        <img src="{{ Vite::asset('{{ $comic['thumb'] }}')}}" alt="Dc Logo"/>
                         <h6>{{ $comic['title'] }}</h6>
                       </div> 
                     @endforeach
