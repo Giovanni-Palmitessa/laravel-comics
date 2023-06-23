f293abb041ce5ac1849abc447cafcd9e99597eae
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-23 16:24:23 +0200

    add footer.blade

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,30 @@
+<div class="top-footer">
+    <div class="container">
+      <div class="links">
+        <div class="link">
+          <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png')}}" />
+          <span>DIGITAL COMICS</span>
+        </div>
+
+        <div class="link">
+          <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png')}}" />
+          <span>DC MERCHANDISE</span>
+        </div>
+
+        <div class="link">
+          <img src="../assets/img/buy-comics-subscriptions.png" />
+          <span>SUBSCRIPTION</span>
+        </div>
+
+        <div class="link">
+          <img src="../assets/img/buy-comics-shop-locator.png" />
+          <span>COMIC SHOP LOCATOR</span>
+        </div>
+
+        <div class="link">
+          <img src="../assets/img/buy-dc-power-visa.svg" />
+          <span>DC POWER VISA</span>
+        </div>
+      </div>
+    </div>
+  </div>
